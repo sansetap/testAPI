7d382042e4fa5d37fc01838585915e65d3484396
sansetap <39578577+sansetap@users.noreply.github.com>
2021-08-02 17:01:18 +0700

    Update CallBack.php

--- CallBack.php
+++ CallBack.php
@@ -1,5 +1,14 @@
 <?php
+$ch = curl_init();
 
-echo "hello W";
-print_r($_REQUEST);
+curl_setopt($ch, CURLOPT_URL, "https://app.clickup.com/api?client_id=W3Q5P30W5K2UUGWQHKA1SEUF4O3C2GOL&redirect_uri=https://testapiclickup.herokuapp.com/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_POST, TRUE);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
 ?>
